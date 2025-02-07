9aaa34d20a2facee89cac679d0ea20274c2ba568
Kaloina RANDRIAMBOLOLONA <kaloinarandria@outlook.fr>
2025-02-07 14:07:44 +0300

    creation service account

--- src/main/java/mg/working/cryptomonnaie/config/FirebaseConfig.java
+++ src/main/java/mg/working/cryptomonnaie/config/FirebaseConfig.java
@@ -5,22 +5,26 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccountKey.json");
+        InputStream serviceAccount = new ClassPathResource("serviceAccountKey.json").getInputStream();
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
 
-        return FirebaseApp.initializeApp(options);
+        if (FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.initializeApp(options);
+        } else {
+            return FirebaseApp.getInstance();
+        }
     }
 }
-
